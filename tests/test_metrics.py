5e67f74a734be9425f767e051eb6907906334714
Gre4ka <hrechkaoleksii@gmail.com>
2026-03-16 13:23:05 +0200

    add iou metric and first tests

---
+++ tests/test_metrics.py
@@ -0,0 +1,19 @@
+from tracker_benchmark.metrics import iou
+
+
+def test_iou_overlap():
+    box_a = [0, 0, 100, 100]
+    box_b = [50, 50, 150, 150]
+
+    result = iou(box_a, box_b)
+
+    assert result > 0
+
+
+def test_iou_no_overlap():
+    box_a = [0, 0, 100, 100]
+    box_b = [200, 200, 300, 300]
+
+    result = iou(box_a, box_b)
+
+    assert result == 0.0
